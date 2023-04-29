db1024616ebaabaee95e72d2bf3965d5189cf76e
lucasHARosa <l.ucas.henrique.a.r@gmail.com>
2023-04-28 23:34:49 -0300

    first commit

---
+++ src/Screens/Report/index.jsx
@@ -0,0 +1,125 @@
+import { Alert, Text, TouchableOpacity, View, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import { styles } from "./styles";
+
+import React, {useState} from "react";
+export function Report() {
+  const options = [{
+    id: 1,
+    text: 'O profissional não compareceu.'
+  },
+  {
+    id: 2,
+    text: 'O profissional não prestou um bom serviço.'    
+  },
+  { 
+    id: 3,
+    text: 'Outro:'
+  }]
+
+
+  const [selected, setSelected] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  function toggleCheckbox(id){
+    const newSelected = [...selected];
+    if(newSelected.includes(id)){
+      newSelected.splice((newSelected.indexOf(id)), 1);
+    }else{
+      newSelected.push(id);
+    }
+    setSelected(newSelected);
+  }
+  function handleReport(){
+    setTitle('');
+    setDescription('');
+    setSelected([]);
+  }
+  function handleOnPress(){
+    let message = '';
+    selected.forEach((id) => {
+      const option = options.find((option) => option.id === id);
+      message += `${option.text}\n`;
+    });
+    
+    if(selected.includes(3)){
+      message += '\n';
+      message += `${title}\n`;
+      message += `${description}\n`;
+    }
+    
+    Alert.alert("Reportando",message,[
+      {
+        text: "Confirmar",
+        onPress: () => handleReport(),
+      },
+      {
+        text: "Cancelar",
+        style: "cancel",
+      }
+    ]);
+  }
+  return(
+    <>
+      
+      <View style={styles.container}>
+
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Icon name="arrowleft" size={30} color="#fff" />
+          </TouchableOpacity>
+          <Text style={styles.titulo}>Reportar</Text>
+          <Icon name="arrowleft" size={16} color="#006557" />
+        </View>
+
+        <View style={styles.containerForm}>
+          <View style={styles.containerCheck}>
+            {options.map((option, index) => (
+              <View key={index} style={styles.checkboxInput}>
+                <TouchableOpacity
+                  style={styles.touchable}
+                  onPress={() => toggleCheckbox(option.id)}
+                >
+                  {selected.includes(option.id) ? 
+                    <Icon name="check" size={20} color="green" /> 
+                    : 
+                    null
+                  }
+                </TouchableOpacity>
+                <Text style={styles.opText}>{option.text}</Text>
+              </View>
+            ))}
+          </View>
+          <View style={styles.containerInput}>
+            <TextInput
+              editable={selected.includes(3)} 
+              style={styles.inputTitle}
+              placeholder="Título"
+              placeholderTextColor="#596462"
+              onChangeText={(text) => setTitle(text)}
+              value={title}
+            />
+            <TextInput
+              editable={selected.includes(3)}
+              style={styles.inputDescription}
+              placeholder="Especifique aqui o que houve."
+              placeholderTextColor="#596462"
+              onChangeText={(text) => setDescription(text)}
+              value={description}
+            />
+          </View>
+          <TouchableOpacity 
+            style={styles.button}
+            disabled={selected.length === 0}
+            onPress={handleOnPress}
+          >
+            <Text style={styles.buttonText}>Reportar</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </>
+  )
+}
+
---
+++ src/Screens/Report/styles.js
@@ -0,0 +1,89 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f8f8f8',
+   
+  },
+  header:{
+    padding:24,
+    marginTop:24,
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    backgroundColor:'#006557',
+  },
+  titulo:{
+    fontWeight:'semibold',
+    fontSize:20,
+    color:'#fff',
+    marginRight:10,
+  },
+  containerForm:{
+    padding:35,
+  },
+  containerCheck:{
+    marginTop:10,
+    
+    gap:24,
+  },
+  checkboxInput:{
+    flexDirection:'row',
+    alignItems:'flex-start',
+  },
+  touchable:{
+    height: 24,
+    width: 24,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor:'#dadada',
+    borderWidth:2,
+    marginTop:3,
+
+  },
+  opText:{
+    marginLeft:12,
+    fontSize:18,
+    fontWeight:'medium',
+    color:'#596462'
+  },
+  containerInput:{
+    marginTop:24,
+    gap:24,
+  },
+  inputTitle:{
+    width:'100%',
+    borderRadius:3,
+    borderWidth:2,
+    borderColor:'#dadada',
+    padding:16,
+  },
+  inputDescription:{
+    width:'100%',
+    height:150,
+    borderRadius:3,
+    borderWidth:2,
+    borderColor:'#dadada',
+    padding:16,
+    textAlignVertical:'top',
+    fontSize:14,
+  },
+  button:{
+    marginTop:30,
+    width:'100%',
+    height:50,
+    borderRadius:5,
+    backgroundColor:'#07689f',
+    justifyContent:'center',
+    alignItems:'center',
+  },
+  buttonText:{
+    fontSize:18,
+    fontWeight:'medium',
+    color:'#fff',
+  },
+
+
+})
